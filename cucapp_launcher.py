232191c7b9d7980d6071b2f68757f00103dcf2bc
Alexandre Wilhelm <contact.dogild@gmail.com>
2014-07-13 22:24:57 -0700

    Added launcher

--- cucapp_launcher.py
+++ cucapp_launcher.py
@@ -4,6 +4,6 @@ import os
 
 if __name__ == "__main__":
 
-    os.system("cd Cucapp-demo-app; ln -s ../features ; ln -s ../Cucapp")
+    os.system("cd Cucapp-demo-app; ln -s ../features ; ln -s ../cucapp")
     os.system("cucumber CUCAPP_APPDIRECTORY=Cucapp-demo-app")
-    os.system("cd Cucapp-demo-app; rm -f features; rm -f Cucapp; rm -f cucumber.html")
+    os.system("cd Cucapp-demo-app; rm -f features; rm -f cucapp; rm -f cucumber.html")
